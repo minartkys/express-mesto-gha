05c7b9dc1f063c1830717bb1ea7f681329b2cc5e
Artyom Minyaev <minart19992@gmail.com>
2022-04-18 16:47:33 +0300

    First rewie 13

--- controllers/cards.js
+++ controllers/cards.js
@@ -29,14 +29,41 @@ module.exports.createCard = (req, res) => {
     });
 };
 
+// eslint-disable-next-line consistent-return
 module.exports.putLike = (req, res) => {
+  if (req.params.cardId !== 24) {
+    return res.status(400).send({
+      message: 'Передан некорректный ID карточки',
+    });
+  }
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
     { new: true },
   )
-    .then((card) => res.send({ data: card }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .then((card) => {
+      if (!card) {
+        return res.status(404).send({
+          message: 'Карточка не обнаружена',
+        });
+      }
+      return res.send(card);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({
+          message: 'Передан некорректный ID карточки',
+        });
+      }
+      if (err.name === 'CastError') {
+        return res.status(404).send({
+          message: 'Карточка не обнаружена',
+        });
+      }
+      return res.status(500).send({
+        message: 'Произошла ошибка',
+      });
+    });
 };
 
 module.exports.deleteLike = (req, res) => {
