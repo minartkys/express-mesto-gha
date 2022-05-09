7726109b0e88464b50a2bff91b0288bce04c562d
Artyom Minyaev <minart19992@gmail.com>
2022-05-09 11:30:30 +0300

    First rewie 14

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const { celebrate, Joi, errors } = require('celebrate');
 const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
+const error = require('./middlewares/error');
 
 const AVATAR_REGEX = /^https?:\/\/(www\.)?[a-zA-Z\d-]+\.[\w\d\-.~:/?#[\]@!$&'()*+,;=]{2,}#?$/;
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
@@ -45,14 +45,14 @@ app.post(
   createUser,
 );
 
-app.use('/', auth, require('./routes/users'));
-app.use('/', auth, require('./routes/cards'));
+app.use('/', require('./routes/users'));
+app.use('/', require('./routes/cards'));
 
 app.use('*', auth, (req, res) => {
   res.status(404).send({ message: 'Запрашиваемый ресурс не найден!' });
 });
 app.use(errors());
-
+app.use(error);
 app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
   // eslint-disable-next-line no-console
