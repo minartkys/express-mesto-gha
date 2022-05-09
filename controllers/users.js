82f7a6fcd028df5a2737a806573a619c2c44796e
Artyom Minyaev <minart19992@gmail.com>
2022-05-09 15:11:34 +0300

    First rewie 14

--- controllers/users.js
+++ controllers/users.js
@@ -38,10 +38,10 @@ module.exports.getUserById = (req, res, next) => {
     });
 };
 
-module.exports.getUsers = (req, res) => {
+module.exports.getUsers = (req, res, next) => {
   User.find({})
     .then((users) => res.send({ data: users }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(next);
 };
 
 module.exports.createUser = (req, res, next) => {
--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,8 @@ const {
 
 router.get('/users', getUsers);
 router.get('/users/:userId', getUserById);
+router.get('/users/me', getUserMe);
 router.patch('/users/me', updateUser);
 router.patch('/users/me/avatar', updateAvatar);
-router.get('/users/me', getUserMe);
+
 module.exports = router;
