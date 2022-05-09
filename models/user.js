a0f11d5c2eceb017f45f3f3bd2be6e635be5b1d4
Artyom Minyaev <minart19992@gmail.com>
2022-05-09 09:47:17 +0300

    First rewie 14

--- models/user.js
+++ models/user.js
@@ -1,43 +1,50 @@
 const mongoose = require('mongoose');
+const { isEmail, isURL } = require('validator');
 const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    default: 'Жак-Ив Кусто',
     minlength: 2,
     maxlength: 30,
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    default: 'Исследователь',
     minlength: 2,
     maxlength: 30,
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
-    default:
-      'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validate: [isURL, 'Ошибка валидации'],
   },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true, select: false },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: [isEmail, 'Ошибка валидации'],
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
 });
 
-// eslint-disable-next-line func-names
 userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email })
-    .select('+password')
-    .then((user) => {
-      if (!user) {
-        throw Error('Неправильные почта или пароль');
+  return this.findOne({ email }).select('+password').then((user) => {
+    if (!user) {
+      throw Error('Неправильные почта или пароль');
+    }
+    return bcrypt.compare(password, user.password).then((matched) => {
+      if (!matched) {
+        throw new Error('Неправильные почта или пароль');
       }
-      return bcrypt.compare(password, user.password).then((matched) => {
-        if (!matched) {
-          throw new Error('Неправильные почта или пароль');
-        }
-        return user;
-      });
+      return user;
     });
+  });
 };
 
-module.exports = mongoose.model('user', userSchema);
+module.exports = mongoose.model('User', userSchema);
