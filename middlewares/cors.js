30789ff2edfaa36f81611dd17660a927e9b71038
Artyom Minyaev <minart19992@gmail.com>
2022-05-13 11:53:59 +0300

    First rewie 15

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -7,7 +7,7 @@ module.exports = (req, res, next) => {
   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
   // проверяем, что источник запроса есть среди разрешённых
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
   }
   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
   // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
