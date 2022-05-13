a0855b6ffcb982b7532abeacd903ea7a7d1f7ef5
Artyom Minyaev <minart19992@gmail.com>
2022-05-13 19:17:14 +0300

    First rewie 15

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ const allowedCors = [
   'http://localhost:3001',
   'http://localhost:3001',
   'https://domainname.students.nomoredomains.xyz',
-  'http://domainname.minartkys.nomoredomains.xyz/',
+  'https://domainname.minartkys.nomoredomains.xyz/',
+  'https://domainname.minartkys.nomoredomains.xyz',
 ];
 app.use(
   cors({
